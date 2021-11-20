f52a0336b188ce0326f4f61ca84215a1720ffd08
tgzss <tobiasgustavoh>
2021-11-20 17:59:51 -0300

    JUnit

--- Desafio.Tobias/src/main/java/com/desafio/tobias/tobias/security/BasicSecurityConfig.java
+++ Desafio.Tobias/src/main/java/com/desafio/tobias/tobias/security/BasicSecurityConfig.java
@@ -38,7 +38,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.authorizeRequests().antMatchers("/clientes/logar").permitAll().antMatchers("/usuarios/cadastrar")
+		http.authorizeRequests().antMatchers("/clientes/logar").permitAll().antMatchers("/clientes/cadastrar")
 				.permitAll().antMatchers(HttpMethod.OPTIONS).permitAll().anyRequest().authenticated().and().httpBasic()
 				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and()
 				.csrf().disable();
